0a0c1b15a4b6e3b85ae3c49c4bea770e4beaeeb4
Jimmy GONCALVES <jgoncalv@e1r4p5.42.fr>
2017-11-05 20:34:21 +0100

    ok

---
+++ first.py
@@ -0,0 +1,33 @@
+import sys
+
+def parse(s):
+	tab = s.partition(',')
+	if not tab[1]:
+		exit("Error: It seems you don't have correctly formated value in theta.txt . t0,t1.")
+	try:
+		t0 = float(tab[0])
+		t1 = float(tab[2])
+	except Exception as e:
+		exit("Error: {}".format(e))
+	return t0, t1
+
+def main():
+	fileName = "theta.txt"
+	t0 = 0
+	t1 = 0
+	try:
+		with open(fileName, 'r') as f:
+			s = f.readline()
+		t0, t1 = parse(s)
+	except Exception as e:
+		print("Error: {}\nSo we use t0 = 0 and t1 = 0\n".format(e))
+	mileage = input("Please enter a mileage :\n")
+	try:
+		mileage = float(mileage)
+	except Exception as e:
+		exit("Error: {}".format(e))
+	print("Estimate price :")
+	print(t0 + (t1 * mileage))
+
+if __name__ == '__main__':
+	main()
