cf2dae89d59c4739a8f4b0d4918cfcedf33146ba
Jimmy GONCALVES <jgoncalv@e1r1p6.42.fr>
2017-11-15 17:21:23 +0100

    il fallait normaliser les datas

--- second.py
+++ second.py
@@ -1,28 +1,26 @@
 #!/usr/bin/python3
 import sys
 import matplotlib.pyplot as plt
-import csv
+import pandas as pd
 
 
 def estim(t0, t1, mil):
 	return t1 * mil + t0
+
+def normalize(lstX, x):
+	return (x - min(lstX)) / (max(lstX) - min(lstX))
 
 def train(mileage, price):
 	t0 = 0.0
 	t1 = 0.0
 	tt0 = 1.0
 	tt1 = 1.0
-	sum0 = 0.0
-	sum1 = 0.0
 	l = len(mileage)
-	print(l)
 	learningRate = 0.01
-	print("ICI ON A = ", sum([(estim(t0, t1, mileage[i]) - price[i]) for i in range(l)]))
-	while abs(tt0) > 0.001 and abs(tt1) > 0.001:
-		for i in range(l):
-			sum0 += (estim(t0, t1, mileage[i]) - price[i])
-			sum1 += (sum0 * mileage[i])
-
+	while abs(tt0) > 0.1 and abs(tt1) > 0.1:
+		# on calcule les sommes
+		sum0 = sum([estim(t0, t1, normalize(mileage, mileage[i])) - price[i] for i in range(l)])
+		sum1 = sum([(estim(t0, t1, normalize(mileage, mileage[i])) - price[i]) * normalize(mileage, mileage[i]) for i in range(l)])
 
 		tt0 = learningRate * sum0 / l
 		tt1 = learningRate * sum1 / l
@@ -32,14 +30,7 @@ def train(mileage, price):
 		t0 -= tt0
 		t1 -= tt1
 
-
-	while(abs(tt0) > 0.001 and abs(tt1) > 0.001):
-		sum0 = sum([self._h(self.data_set[i][0] + self.scaled_min) - self.data_set[i][1] for i in range(len(self.data_set))])
-		sum1 = sum([(self._h(self.data_set[i][0] + self.scaled_min) - self.data_set[i][1]) * self.data_set[i][0] for i in range(len(self.data_set))])
-		tt0 = learningRate * sum0 / len(self.data_set)
-		tt1 = learningRate * sum1 / len(self.data_set)
-		self.theta0 -= tmp_theta0
-		self.theta1 -= tmp_theta1
+	return t0, t1
 
 	print(t0, t1)
 	print(estim(-1.4165536138106417e+293, -1.808206190753816e+299, 250000))
@@ -52,27 +43,18 @@ def display(mileage, price, lineMileage, linePrice):
 	plt.show()
 
 def main():
-	dataFileToOpen = "data.csv"
 	data = []
 	try:
-		with open(dataFileToOpen, 'r') as f:
-			reader = csv.reader(f)
-			data = list(reader)
+		data = pd.read_csv("data.csv", dtype = "float")
 	except Exception as e:
 		exit(e)
-	if data[0] == ['km', 'price']:
-		data.remove(data[0])
-	for d in data:
-		for i, val in enumerate(d):
-			try:
-				d[i] = float(val)
-			except Exception as e:
-				exit(e)
-	mileage = [x[0] for x in data]
-	price = [x[1] for x in data]
-	if len(mileage) != len(price):
-		exit("Missing information for price or mileage.")
-	train(mileage, price)
+	minPrice = min(data.price)
+	maxPrice = max(data.price)
+	minMileage = min(data.km)
+	maxMileage = max(data.km)
+	mileage = (data.km - minMileage) / (maxMileage - minMileage)
+	price = (data.price - minPrice) / (maxPrice - minPrice)
+	t0, t1 = train(mileage, price)
 
 if __name__ == '__main__':
 	main()
